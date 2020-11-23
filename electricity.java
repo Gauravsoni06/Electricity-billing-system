c648dd4e3aa8201c3aa52acb1e9c284c15bd058e
Gaurav <67866172+Gauravsoni06@users.noreply.github.com>
2020-11-23 13:15:56 +0530

    Add files via upload

---
+++ electricity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackage;
+
+
+/**
+ *
+ * @author lenovo
+ */
+
+public interface electricity
+{
+	public void calculate(int id);
+}
